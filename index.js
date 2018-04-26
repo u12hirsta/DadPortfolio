e7ea5a7a46e37480eb9938b5290a4e92e742e8a5
Andrew Hirst <u12hirsta@cfgslive.com>
2018-04-26 17:44:06 +0100

    Added new Background

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 document.getElementById("btnClose").onclick = function(){
   if(document.getElementById("sidenav").style.display == 'none'){
     document.getElementById("sidenav").style.display = 'block';
-    document.getElementsByClassName("sideButton")[0].style.right = '98px';
-    document.getElementsByClassName("sideButton")[0].style.background = 'none';
+    document.getElementsByClassName("sideButton")[0].style.right = '125px';
     document.getElementById("arrow").className = "fa fa-arrow-right";
   }else {
     document.getElementById("sidenav").style.display = 'none';
-    document.getElementsByClassName("sideButton")[0].style.right = '10px';
-    document.getElementsByClassName("sideButton")[0].style.background = 'rgba(0, 200, 255, 0.7)';
+    document.getElementsByClassName("sideButton")[0].style.right = '0';
     document.getElementById("arrow").className = "fa fa-arrow-left";
   }
 }
